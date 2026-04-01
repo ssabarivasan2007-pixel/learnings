446422c55fe0731364282503d782876554c79c5b
Sabarivasan S <s.sabarivasan2007@gmail.com>
2026-04-01 21:02:45 +0530

    finally it gets completed

--- collection.js
+++ collection.js
@@ -15,3 +15,12 @@ search.addEventListener("keyup",function(event){
         }
     }
 });
+var sideNavbar = document.querySelector(".side-navbar");
+var menu = document.getElementsByClassName("navbar-menu-toggle")[0];
+var close=document.getElementsByClassName("fa-xmark")[0];
+menu.addEventListener("click", function() {
+    sideNavbar.style.left = "0";
+});
+close.addEventListener("click", function() {
+    sideNavbar.style.left = "-100%";
+})
